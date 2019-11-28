940efd44d3629f0048ae03cc32cb7b959b072130
Luke Gardener <luke.g@bitocean.com>
2019-11-28 13:29:30 +0900

    Added sorted binary tree

--- src/main/java/com/arkumbra/ds/tree/SortedBinaryTree.java
+++ src/main/java/com/arkumbra/ds/tree/SortedBinaryTree.java
@@ -7,11 +7,14 @@ import java.util.Iterator;
  * See also: TreeSet
  * @param <T>
  */
-public class SortedBinaryTree<T> implements Tree<T> {
+public class SortedBinaryTree<T> implements Tree<T>, Iterable<T> {
 
   private SortedBinaryTree<T> leafLeft;
   private SortedBinaryTree<T> leafRight;
   private T val;
+
+  // used for iteration
+  private SortedBinaryTree<T> parent;
 
   private Comparator<T> comparator;
 
@@ -19,30 +22,30 @@ public class SortedBinaryTree<T> implements Tree<T> {
     this.comparator = comparator;
   }
 
+  public SortedBinaryTree(Comparator<T> comparator, SortedBinaryTree<T> parent) {
+    this.comparator = comparator;
+    this.parent = parent;
+  }
+
 
   @Override
   public void add(T t) {
     if (val == null) {
       this.val = t;
-
     } else {
       pushDownToLeaves(t);
     }
-
   }
 
   private void pushDownToLeaves(T t) {
     int compared = comparator.compare(t, this.val);
     if (compared < 0) {
       pushLeft(t);
-
     } else if (compared == 0) {
       pushLeft(t);
       //throw new RuntimeException("Hello test");
-
     } else {
       pushRight(t);
-
     }
   }
 
@@ -54,7 +57,7 @@ public class SortedBinaryTree<T> implements Tree<T> {
 
   private void createLeftNodeIfDoesntExist() {
     if (leafLeft == null) {
-      leafLeft = new SortedBinaryTree<>(comparator);
+      leafLeft = new SortedBinaryTree<>(comparator, this);
     }
   }
 
@@ -66,7 +69,7 @@ public class SortedBinaryTree<T> implements Tree<T> {
 
   private void createRightNodeIfDoesntExist() {
     if (leafRight == null) {
-      leafRight = new SortedBinaryTree<>(comparator);
+      leafRight = new SortedBinaryTree<>(comparator, this);
     }
   }
 
@@ -94,8 +97,63 @@ public class SortedBinaryTree<T> implements Tree<T> {
     return sb.toString();
   }
 
-  //  @Override
-//  public Iterator<T> iterator() {
-//    return null;
-//  }
+  @Override
+  public Iterator<T> iterator() {
+    return new SortedBinaryTreeIterator(this);
+  }
+
+
+
+  class SortedBinaryTreeIterator implements Iterator<T> {
+    private SortedBinaryTree<T> next;
+
+    public SortedBinaryTreeIterator(SortedBinaryTree<T> root) {
+      this.next = getLeftMost(root);
+    }
+
+    @Override
+    public boolean hasNext() {
+      return next != null;
+    }
+
+    @Override
+    public T next() {
+      T nextVal = next.val;
+
+      // https://stackoverflow.com/questions/12850889/in-order-iterator-for-binary-tree
+      // If you can walk right, walk right, then fully left.
+      if (next.leafRight != null) {
+        next = getLeftMost(next.leafRight);
+        return nextVal;
+      }
+
+      // otherwise, walk up until you come from left.
+      while (true) {
+        // if we're at the root/parent node, go down the right
+        if (next.parent == null) {
+          next = null;
+          return nextVal;
+        }
+
+        // If the parent's left leaf is the same as current, then means we're walking up the left
+        else if (next == next.parent.leafLeft) {
+          next = next.parent;
+          return nextVal;
+
+        } else {
+          next = next.parent;
+        }
+      }
+    }
+
+    private SortedBinaryTree<T> getLeftMost(SortedBinaryTree<T> node) {
+      SortedBinaryTree<T> leftMost = node;
+      while (leftMost.leafLeft != null) {
+        leftMost = leftMost.leafLeft;
+      }
+      return leftMost;
+    }
+
+  }
+
 }
--- src/test/java/com/arkumbra/ds/tree/SortedBinaryTreeTest.java
+++ src/test/java/com/arkumbra/ds/tree/SortedBinaryTreeTest.java
@@ -1,41 +1,58 @@
 package com.arkumbra.ds.tree;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SortedBinaryTreeTest {
-  private Tree<Integer> tree;
+  private SortedBinaryTree<Integer> tree;
 
   @Before
   public void setUp() {
-    this.tree = new SortedBinaryTree<>(((o1, o2) -> o1-o2));
+//    this.tree = new SortedBinaryTree<>(((o1, o2) -> o1-o2));
+    this.tree = new SortedBinaryTree<>(new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        if (o1 == o2) {
+          return 0;
+        } else if (o1 < o2) {
+          return -1;
+        } else {
+          return 1;
+        }
+      }
+    });
   }
 
   @Test
   public void test() {
-    tree.add(3);
-    tree.add(1);
-    tree.add(7);
-    tree.add(7);
-    tree.add(7);
-    tree.add(2);
-    tree.add(5);
-    tree.add(8);
-    tree.add(9);
-    tree.add(9);
-    tree.add(9);
-    tree.add(10);
-    tree.add(11);
-    tree.add(11);
-    tree.add(59);
-    tree.add(59);
-    tree.add(22);
+    Random r = new Random();
 
+    for (int i = 0; i < 1000; i++) {
+      tree.add(r.nextInt());
+    }
+    //System.out.println("Tree is " + tree + "\n");
 
-    System.out.println(tree);
+    // Check results
+    Long sum = 0L;
+    Integer prev = null;
+    for (Integer i : tree) {
+      sum += i;
+      //System.out.println("Found " + i);
 
+      if (prev == null) {
+        prev = i;
+      } else {
+        assertTrue("Previous number " + prev + ", but current " + i, i > prev);
+      }
 
+    }
+
+    System.out.println("Total value of tree " + sum);
   }
 
 }
